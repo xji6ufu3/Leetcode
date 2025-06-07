84b61aa5deb272837e8b9d789b358f3d938ffc24
max2124 <max93.01.11@gmail.com>
2025-06-07 16:01:59 +0800

    modified 217.c

--- code/217.c
+++ code/217.c
@@ -1,3 +1,10 @@
+/**
+ * 只想到兩種做法
+ * 1. 先 sort 再找重複，time: O(nlogn), space: O(1)
+ * 2. 用 hash，但 C 的話好像只能開 bool hash[2*1e9];，雖然時間 O(n)，但空間 O(n) 太大
+ * 2*1e9 byte 約為 2GB，消耗太多空間
+ */
+
 int cmp(const void *a, const void *b){
     return *(int*)a - *(int*)b;
 }
