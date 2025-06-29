6f3341cb2df800ee189c4cc17e9ee48bc19f4876
max2124 <max93.01.11@gmail.com>
2025-06-29 13:45:30 +0800

    modified 257.c

--- code/257.c
+++ code/257.c
@@ -14,37 +14,27 @@ bool isleaf(struct TreeNode* node){
     return node->left == NULL && node->right == NULL;
 }
 
-void traverse(struct TreeNode* node, char* str, int* cnt, char** ans){
-    if(node == NULL) return;
-    char tmp[10];
-    sprintf(tmp, "->%d", node->val);
-    strcat(str, tmp);
-    if(isleaf(node)){
-        ans[*cnt] = malloc(strlen(str)+1);
-        strcpy(ans[(*cnt)++], str);
+void dfs(struct TreeNode* node, char* path, char** ans, int* cnt) {
+    if(!node) return;
+    
+    char newpath[605];
+    if (!*path) sprintf(newpath, "%d", node->val);
+    else sprintf(newpath, "%s->%d", path, node->val);
+
+    if (isleaf(node)) {
+        ans[*cnt] = malloc(strlen(newpath) + 1);
+        strcpy(ans[(*cnt)++], newpath);
         return;
     }
-    char strtmp[605];
-    strcpy(strtmp, str);
-    traverse(node->left, str, cnt, ans);
-    traverse(node->right, strtmp, cnt, ans);
+
+    dfs(node->left, newpath, ans, cnt);
+    dfs(node->right, newpath, ans, cnt);
 }
 
 char** binaryTreePaths(struct TreeNode* root, int* returnSize) {
-    char** ans = malloc(sizeof(char*)*100);
-    int cnt = 0;
-    char str[605];
-    sprintf(str, "%d", root->val);
-    if(isleaf(root)){
-        ans[cnt] = malloc(strlen(str)+1);
-        sprintf(ans[cnt++], "%s", str);
-        *returnSize = cnt;
-        return ans;
-    }
-    char strtmp[605];
-    strcpy(strtmp, str);
-    traverse(root->left, str, &cnt, ans);
-    traverse(root->right, strtmp, &cnt, ans);
-    *returnSize = cnt;
+    char** ans = malloc(sizeof(char*) * 100);
+    char path[1] = "";
+    *returnSize = 0;
+    dfs(root, path, ans, returnSize);
     return ans;
 }
