3ed7536fd58fbdafc275b7104298fc380fdf4bff
max2124 <max93.01.11@gmail.com>
2025-08-26 10:52:00 +0800

    add 3392.cpp

---
+++ code/3392.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int cnt = 0;
+        for(int i = 1; i < nums.size()-1; ++i) {
+            if(nums[i] % 2 == 0 && nums[i-1] + nums[i+1] == nums[i]/2) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
