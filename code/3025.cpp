cce68bd5fad905bd705f7f45af0c72be8cb417da
max2124 <max93.01.11@gmail.com>
2025-09-02 17:39:30 +0800

    add 3025.cpp

---
+++ code/3025.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+                
+                if (x1 >= x2 || y1 <= y2) continue;
+                
+                bool valid = true;
+                for (int k = 0; k < n; k++) {
+                    if (k == i || k == j) continue;
+                    
+                    int x3 = points[k][0], y3 = points[k][1];
+                    
+                    if (x3 >= x1 && x3 <= x2 && y3 >= y2 && y3 <= y1) {
+                        valid = false;
+                        break;
+                    }
+                }
+                
+                if (valid) count++;
+            }
+        }
+        
+        return count;
+    }
+};
