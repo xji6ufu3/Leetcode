883ce8c4eaf5cd463b96b485ea1d34b9aca07061
max2124 <max93.01.11@gmail.com>
2025-11-23 02:02:35 +0800

    add 3190.cpp

---
+++ code/3190.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] % 3) ++cnt;
+        }
+        return cnt;
+    }
+};
