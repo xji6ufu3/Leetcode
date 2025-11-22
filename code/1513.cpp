cc3e49490200489b8a2407c12539d090e56d7123
max2124 <max93.01.11@gmail.com>
2025-11-23 05:52:16 +0800

    add some comment in 1513.cpp

--- code/1513.cpp
+++ code/1513.cpp
@@ -5,6 +5,11 @@ using namespace std;
 class Solution {
 public:
     int numSub(string s) {
+        // 11 -> 2*'1' + 1*'11' = 2+1
+        // 111 -> 3*'1' + 2*'11' + 1*'111' = 3+2+1
+        // 1111 -> 4*'1' + 3*'11' + 2*'111' + 1*'1111' = 4*3*2*1
+        // so, if there is n 1's
+        // the number of substrings with only 1's is n*(n+1) / 2
         long long ans = 0, cnt = 0, MOD = 1e9 + 7;
         for(int i = 0; i < s.length(); ++i){
             if(s[i] == '1'){
